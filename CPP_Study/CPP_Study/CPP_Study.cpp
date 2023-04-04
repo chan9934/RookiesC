d9a5dd24aa8b6f7d1a5735451cf6215719dd1758
chan9934 <106580671+chan9934@users.noreply.github.com>
2023-04-04 21:44:18 +0900

    [김동찬]

--- CPP_Study/CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study/CPP_Study.cpp
@@ -2,13 +2,18 @@
 
 using namespace std;
 
-// 데이터를 메모리에 할당하고 가공하는 방법에 대해 알아봤다.
-// 가공한 데이터를 이용해서 무엇인가를 하고 싶다면?
 
-int hp = 100;
 
 int main()
 {
-	
-	
+	// 1-10사이의 홀수 출력
+
+	for (int count = 1; count <= 10; count++)
+	{
+		bool isEven = ((count % 2) == 0);
+
+		if (isEven)
+			continue;
+		cout << count << endl;
+	}
 }
