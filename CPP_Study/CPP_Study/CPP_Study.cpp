f81bee9ef20f0d9d897a94164f59a3d50bd58879
chan9934 <106580671+chan9934@users.noreply.github.com>
2023-04-21 08:25:41 +0900

    [김동찬] 연습문제

--- CPP_Study/CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study/CPP_Study.cpp
@@ -15,16 +15,101 @@ int StrLen(char msg[])
 		}
 		++Count;
 	}
+
 	return Count;
 
+}
+
+char* StrCpy(char* des, char* src)
+{
+	int count = 0;
+	while (src[count] != '\0')
+	{
+		des[count] = src[count];
+		++count;
+	}
+	des[count] = src[count];
+
+	return des;
+
+}
+
+char* Cpy(char* des, char* src)
+{
+	
+
+	StrCpy(des + StrLen(des), src);
+
+	return des;
+	
+}
+
+int StrCmp(char* a, char* b)
+{
+	char* first = nullptr;
+	char* second = nullptr;
+	if (StrLen(a) > StrLen(b))
+	{
+		first = a;
+		second = b;
+	}
+
+	else
+	{
+		first = b;
+		second = a;
+	}
+	
+	for (int i = 0; i < StrLen(first); ++i)
+	{
+		bool Last = false;
+		if ((i + 1) == StrLen(first))
+		{
+			Last = true;
+		}
+		if (a[i] > b[i])
+		{
+			return 1;
+		}
+		else if (a[i] == b[i])
+		{
+			if (Last)
+			{
+				return 0;
+			}
+			continue;
+		}
+		else
+		{
+			return -1;
+		}
+	}
+
+}
+
+char* ReverseString(char* a)
+{
+	char Temp[BUF_SIZE];
+
+	StrCpy(Temp, a);
+	int Count = StrLen(a);
+	for (int i = 0; i < Count; ++i)
+	{
+		a[i] = Temp[Count - 1 - i];
+	}
+	a[Count] = '\0';
+
+	
+	return a;
 }
 
 int main()
 {
 	
-	char a[BUF_SIZE] = "Hello";
+	char a[BUF_SIZE] = "aa";
 
-	cout << sizeof(a) << endl;
+	char b[BUF_SIZE] = "a";
 
-	cout << StrLen(a);
+	char c[BUF_SIZE] = "HelloWorld";
+	cout << ReverseString(c);
 }
