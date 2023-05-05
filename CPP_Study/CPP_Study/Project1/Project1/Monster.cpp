d6e6fb1ec937045eb00570e63eb8ed74f2c392a9
chan9934 <106580671+chan9934@users.noreply.github.com>
2023-05-05 23:53:48 +0900

    [김동찬]

--- CPP_Study/CPP_Study/Project1/Project1/Monster.cpp
+++ CPP_Study/CPP_Study/Project1/Project1/Monster.cpp
@@ -1,10 +1,1 @@
 #include "Monster.h"
-#include <iostream>
-
-using namespace std;
-void Monster::PrintInfo()
-{
-	cout << "--------------------------" << endl;
-	cout << "[ 몬스터 정보 ] " << "Hp : " << m_Hp << "Attack : " << m_Attack << "Defence : " << m_Defence << endl;
-	cout << "--------------------------" << endl;
-}
