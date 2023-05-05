266660d456afa2dbfdd7c451edef0a6d92d72f62
chan9934 <106580671+chan9934@users.noreply.github.com>
2023-05-06 05:53:22 +0900

    [김동찬]

--- CPP_Study/CPP_Study/Project1/Project1/CPP_Study.cpp
+++ CPP_Study/CPP_Study/Project1/Project1/CPP_Study.cpp
@@ -1,17 +1,46 @@
-﻿#include<iostream>
-#include"Game.h"
+﻿#include <iostream>
 
 using namespace std;
 
+
+class Knight
+{
+public:
+	int Hp = 3;
+};
+
+template<typename T1, typename T2>
+void Print(T1 a, T2 b)
+{
+	cout << a << endl;
+	cout << b << endl;
+}
+
+template<typename T>
+void Print(T a)
+{
+	cout << a << endl;
+}
+
+template<>
+void Print(Knight k1)
+{
+	cout << k1.Hp << endl;
+}
+//
+//ostream& operator<<(ostream& os, const Knight& k2)
+//{
+//	os << k2.Hp << endl;
+//
+//	return os;
+//}
 int main()
 {
-	srand((unsigned int)(time(nullptr)));
-	Game game;
-
-	game.Init();
-
-	while (true)
-	{
-		game.Update();
-	}
+	/*
+	Print(1, 2);
+	Print("안녕하세요", 2);
+	Print(1.0f, 2);
+	Print(3);*/
+	Knight k1;
+	Print(k1);
 }
