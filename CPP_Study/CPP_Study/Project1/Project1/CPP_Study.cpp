c259fb11cee586752a757e8b0ed49d64f955d5aa
chan9934 <106580671+chan9934@users.noreply.github.com>
2023-05-07 14:55:59 +0900

    [김동찬]

--- CPP_Study/CPP_Study/Project1/Project1/CPP_Study.cpp
+++ CPP_Study/CPP_Study/Project1/Project1/CPP_Study.cpp
@@ -1,46 +1,18 @@
 ﻿#include <iostream>
+#include <vector>
 
 using namespace std;
 
-
-class Knight
-{
-public:
-	int Hp = 3;
-};
-
-template<typename T1, typename T2>
-void Print(T1 a, T2 b)
-{
-	cout << a << endl;
-	cout << b << endl;
-}
-
-template<typename T>
-void Print(T a)
-{
-	cout << a << endl;
-}
-
-template<>
-void Print(Knight k1)
-{
-	cout << k1.Hp << endl;
-}
-//
-//ostream& operator<<(ostream& os, const Knight& k2)
-//{
-//	os << k2.Hp << endl;
-//
-//	return os;
-//}
 int main()
 {
-	/*
-	Print(1, 2);
-	Print("안녕하세요", 2);
-	Print(1.0f, 2);
-	Print(3);*/
-	Knight k1;
-	Print(k1);
+	vector<int> V;
+
+	
+	
+	V.resize(1000);
+	for (int i = 0; i < 1000; ++i)
+	{
+		V.push_back(100);
+		cout << V.size() << " " << V.capacity() << endl;
+	}
 }
