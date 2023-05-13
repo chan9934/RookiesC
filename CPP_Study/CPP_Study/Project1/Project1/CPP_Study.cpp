ee8635c9c996d3860537b02ec8f76ad80b80074a
chan9934 <106580671+chan9934@users.noreply.github.com>
2023-05-13 13:22:05 +0900

    [김동찬]

--- CPP_Study/CPP_Study/Project1/Project1/CPP_Study.cpp
+++ CPP_Study/CPP_Study/Project1/Project1/CPP_Study.cpp
@@ -1,37 +1,16 @@
 ﻿#include <iostream>
-#include<set>
 
 using namespace std;
 
+class knight
+{
+public:
+	knight(initializer_list<int> it)
+	{
+
+	}
+};
 int main()
 {
-	set<int> S;
-
-	S.insert(10);
-	S.insert(20);
-	S.insert(30);
-	S.insert(40);
-	S.insert(50);
-	S.insert(60);
-	S.insert(70);
-	S.insert(80);
-	S.insert(90);
-	S.insert(100);
-
-	S.erase(40);
-
-	set<int>::iterator SF = S.find(50);
-	if (SF != S.end())
-	{
-		cout << "찾음" << endl;
-	}
-	else
-	{
-		cout << "못 찾음" << endl;
-	}
-
-	for (set<int>::iterator it = S.begin(); it != S.end(); ++it)
-	{
-		cout << *it << endl;
-	}
+	knight k1{ 1,2,3,4,5 };
 }
