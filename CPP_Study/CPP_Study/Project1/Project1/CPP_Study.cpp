cf273bdee078be791affb2909317be8f2e162fc3
chan9934 <106580671+chan9934@users.noreply.github.com>
2023-05-15 06:59:01 +0900

    [김동찬]

--- CPP_Study/CPP_Study/Project1/Project1/CPP_Study.cpp
+++ CPP_Study/CPP_Study/Project1/Project1/CPP_Study.cpp
@@ -1,71 +1,142 @@
 ﻿#include <iostream>
+#include<vector>
 
 using namespace std;
 
-class Pet
+enum class ItemType
 {
-
+	None,
+	Armor,
+	Weapon,
+	Jewelry,
+	Consumable
 };
 
-class Knight
+enum class Rarity
 {
-public:
-	Knight()
-	{
-	}
-	~Knight()
-	{
-		if (m_Pet)
-		{
-			delete m_Pet;	
-		}
-	}
-	Knight(const Knight& _Knight)
-	{
-	}
-
-	void Printf()
-	{
-
-	}
-
-	void operator=(const Knight& _Knight)
-	{
-		m_Pet = _Knight.m_Pet;
-		m_Hp = _Knight.m_Hp;
-	}
-
-public:
-	Pet* m_Pet = nullptr;
-	int m_Hp = 100;
+	Common,
+	Rare,
+	Unique
 };
 
-// lvalue : 단일식을 넘어서 계속 지속되는 개체
-// rvalue : lvalue가 아닌 나머지
-
-void Knight_Copy(Knight _Knight)
+class Item
 {
-	_Knight.m_Hp = 100;
-}
+public:
+	Item()
+	{
 
-void Knight_LvalueRef(Knight& _Knight)
-{
+	}
 
-}
+	Item(int _ItemId, ItemType _Type, Rarity _Rarity)
+		: m_ItemId(_ItemId)
+		, m_Type(_Type)
+		, m_Rarity(_Rarity)
+	{
 
-void Knight_constLvalueRef(const Knight& _Knight)
-{
+	}
+	
+public:
+	int m_ItemId = 0;
+	ItemType m_Type = ItemType::None;
+	Rarity m_Rarity = Rarity::Common;
+};
 
-}
-
-void Knight_RvalueRef(Knight&& _Knight)
-{
-	_Knight.m_Hp = 100;
-	_Knight.Printf();
-}
 int main()
 {
-	Knight k1;
-	Knight_RvalueRef(Knight());
-	Knight_RvalueRef(static_cast<Knight&&>(k1));
+	vector<Item> v;
+	v.push_back(Item(1, ItemType::Weapon, Rarity::Common));
+	v.push_back(Item(2, ItemType::Armor, Rarity::Common));
+	v.push_back(Item(3, ItemType::Jewelry, Rarity::Rare));
+	v.push_back(Item(4, ItemType::Weapon, Rarity::Unique));
+
+	{
+		//struct IsUniqueItem
+		//{
+		//	bool operator()(const Item& _Item)
+		//	{
+		//		return _Item.m_Rarity == Rarity::Unique;
+		//	}
+		//};
+
+		//// 클로져(Closure)
+		//auto lambda = [](const Item& _Item) -> bool
+		//{
+		//	return _Item.m_Rarity == Rarity::Unique;
+		//};
+
+		struct FindItemByItemId
+		{
+			FindItemByItemId(int _ItemID)
+				: m_ItemID(_ItemID)
+			{
+			}
+
+			bool operator()(const Item& _Item)
+			{
+				return _Item.m_ItemId == m_ItemID;
+			}
+
+		public:
+			int m_ItemID;
+		};
+
+		// [ ] 캡쳐 (Capture) : 함수 객체 내부에 변수를 저장하는 개념과 유사
+		// 사진을 찰칵 [캡쳐] 하듯 일종의 스냅샷을 찍는다고 이해
+		// 기본 캡쳐 모드 : 값(복사) 방식(=) 참조 방식 (&)
+	/*	int _ItemId = 4;
+		auto Lambda = [&](const Item& _Item) -> bool {return _Item.m_ItemId == _ItemId; };
+		_ItemId = 10;*/
+
+		struct FindItem
+		{
+		public:
+			FindItem(int _ItemId, Rarity _Rarity, ItemType _ItemType)
+				: m_ItemId(_ItemId)
+				, m_Rarity(_Rarity)
+				, m_ItemType(_ItemType)
+			{
+
+			}
+
+			bool operator()(const Item& _Item)
+			{
+				return m_ItemId == _Item.m_ItemId && m_Rarity == _Item.m_Rarity && m_ItemType == _Item.m_Type;
+			}
+		public:
+			int m_ItemId;
+			Rarity m_Rarity;
+			ItemType m_ItemType;
+		};
+
+		int T_ItemId = 4;
+		Rarity T_Rarity = Rarity::Unique;
+		ItemType T_Type = ItemType::Weapon;
+		auto Labmda = [&](const Item& _Item)->bool {return T_ItemId == _Item.m_ItemId && T_Rarity == _Item.m_Rarity && T_Type == _Item.m_Type; };
+		auto Findit = std::find_if(v.begin(), v.end(),Labmda);
+		if (Findit != v.end())
+		{
+			cout << "아이템 ID : " << Findit->m_ItemId << endl;
+		}
+	}
+	{
+		class Knight
+		{
+		public:
+			auto ResetHpJob()
+			{
+				return [=]()
+				{
+					m_Hp = 100;
+				};
+			}
+		public:
+			int m_Hp = 100;
+		};
+
+
+		Knight* k = new Knight();
+		auto job = k->ResetHpJob();
+		job();
+	}
+	
 }
