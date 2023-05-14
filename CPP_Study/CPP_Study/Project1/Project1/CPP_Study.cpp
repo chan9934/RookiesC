d0db70d57fc20c2c7d384cbd92004746f97182d4
chan9934 <106580671+chan9934@users.noreply.github.com>
2023-05-14 14:56:00 +0900

    [김동찬] 오른값 참조

--- CPP_Study/CPP_Study/Project1/Project1/CPP_Study.cpp
+++ CPP_Study/CPP_Study/Project1/Project1/CPP_Study.cpp
@@ -2,15 +2,70 @@
 
 using namespace std;
 
-class knight
+class Pet
+{
+
+};
+
+class Knight
 {
 public:
-	knight(initializer_list<int> it)
+	Knight()
+	{
+	}
+	~Knight()
+	{
+		if (m_Pet)
+		{
+			delete m_Pet;	
+		}
+	}
+	Knight(const Knight& _Knight)
+	{
+	}
+
+	void Printf()
 	{
 
 	}
+
+	void operator=(const Knight& _Knight)
+	{
+		m_Pet = _Knight.m_Pet;
+		m_Hp = _Knight.m_Hp;
+	}
+
+public:
+	Pet* m_Pet = nullptr;
+	int m_Hp = 100;
 };
+
+// lvalue : 단일식을 넘어서 계속 지속되는 개체
+// rvalue : lvalue가 아닌 나머지
+
+void Knight_Copy(Knight _Knight)
+{
+	_Knight.m_Hp = 100;
+}
+
+void Knight_LvalueRef(Knight& _Knight)
+{
+
+}
+
+void Knight_constLvalueRef(const Knight& _Knight)
+{
+
+}
+
+void Knight_RvalueRef(Knight&& _Knight)
+{
+	_Knight.m_Hp = 100;
+	_Knight.Printf();
+}
 int main()
 {
-	knight k1{ 1,2,3,4,5 };
+	Knight k1;
+	Knight_RvalueRef(Knight());
+	Knight_RvalueRef(static_cast<Knight&&>(k1));
 }
