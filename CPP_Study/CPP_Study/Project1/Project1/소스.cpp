a74a463d8f5b33c16b76a9e0d9f4eceed97dcce3
chan9934 <106580671+chan9934@users.noreply.github.com>
2023-08-25 08:05:21 +0900

    [김동찬]

--- CPP_Study/CPP_Study/Project1/Project1/소스.cpp
+++ CPP_Study/CPP_Study/Project1/Project1/소스.cpp
@@ -2,22 +2,28 @@
 
 using namespace std;
 
-void jump() {};
-void fireGun() {};
-void swapWeapon() {};
-void lurchIneffectively() {};
-
-class InputHandler
+class FileSystem
 {
 public:
-	InputHandler();
-
-	void HandleInput()
+	static FileSystem& Instance()
 	{
-		
+		static FileSystem* _instance = new FileSystem();
+		return *_instance;
 	}
+	int a = 32;
+
+private:
+	FileSystem(){ }
+
 };
+
 int main()
 {
+	FileSystem& fs1 = FileSystem::Instance();
+	FileSystem& fs2 = FileSystem::Instance();
+	cout << fs1.a << endl;
+	fs1.a = 5;
+	cout << fs1.a << endl;
+	cout << fs2.a << endl;
 
 }
