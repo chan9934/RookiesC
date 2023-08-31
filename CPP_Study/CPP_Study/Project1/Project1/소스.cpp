0588af23873834763ee025ba523a4178c1312960
chan9934 <106580671+chan9934@users.noreply.github.com>
2023-09-01 07:39:08 +0900

    공부
    이중버퍼

--- CPP_Study/CPP_Study/Project1/Project1/소스.cpp
+++ CPP_Study/CPP_Study/Project1/Project1/소스.cpp
@@ -1,15 +1,30 @@
-class GameObject
+#include <iostream>
+
+using namespace std;
+
+class Framebuffer
 {
-protected:
-	Log& getLog() { return log_; }
+public:
+	Framebuffer()
+	{
+		clear();
+	}
+	void clear()
+	{
+		for (int i = 0; i < WIDTH * HEIGHT; ++i)
+		{
+			pixels_[i] = WHITE;
+		}
+	}
+	void draw(int x, int y)
+	{
+		pixels_[(WIDTH * y) + x] = BLACK;
+	}
+	const char* getPixels() { return pixels_; }
 
 private:
-	static Log& log_;
-};
-class Enemy : public GameObject
-{
-	void doSomething()
-	{
-		getLog().write("IO canLog!");
-	}
+	static const int WIDTH = 160;
+	static const int HEIGHT = 120;
+
+	char pixels_[WIDTH * HEIGHT];
 };
