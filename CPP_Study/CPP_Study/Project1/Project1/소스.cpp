1c143a9d45d1b6cd97b4082ce86bc2bf09bfa561
chan9934 <106580671+chan9934@users.noreply.github.com>
2023-09-07 00:21:51 +0900

    공부 바이트코드

--- CPP_Study/CPP_Study/Project1/Project1/소스.cpp
+++ CPP_Study/CPP_Study/Project1/Project1/소스.cpp
@@ -1,53 +1,84 @@
-class Scene
-{
-public:
-	Scene() : current_(buffer_[0]), next_(&buffers_[1]) {}
-	void draw()
-	{
-		next_->clear();
-		next_->draw(1, 1);
-		swap();
-	}
-	Framebuffer& getBuffer(){returm * curerent)}
-	swap)
 
+
+enum Instruction
+{
+	INST_SET_HEALTH = 0x00,
+	INST_SET_WISDOM = 0x01,
+	INST_SET_AGILITY = 0x02,
+	INST_SET_SOUND = 0x03,
+	INST_SET_PARTICLES = 0x04,
 
 };
-while (true)
+
+
+class VM
 {
-	double start = getCurrentIme();
-	processInput();
-	update();
-	render();
-	sleep(start + MS_PER_Frame - get CurrentTime());
-
-}
-
-double lastTime = getCurrentTime();
-while (true)
-{
-	double currnet = getCurrentTime();
-	double elapsed = crueent - lastTime;
-	processInput();
-	update(elapsed);
-	render();
-	lastTime = current;
-}
-
-double previous = getCurrentTime();
-double lag = 0.0;
-while (true)
-{
-	double current - getCurrentTime();
-	double elapsed = cureent - previous;
-	previous = current;
-	lag += elapsed;
-	processInput();
-
-	while (lag >= MS_PER_UPDATE)
+public:
+	VM() : stackSize_(0) {}
+	void interpret(char bytecode[], int size)
 	{
-		update();
-		lag -= MS_PER_UPDATE;
+		for (int i = 0; i < size; ++i)
+		{
+			char instruction = bytecode[i];
+
+			switch (instruction)
+			{
+			case INST_SET_HEALTH:
+			{
+				int amount = pop();
+				int wizard = pop();
+				setHealth(wizard, amount);
+				break;
+			}
+			case INST_SET_WISDOM:
+				setWisdom(0, 100);
+				break;
+			case INST_SET_AGILITY:
+				setAgility(0, 100);
+				break;
+			case INST_SET_SOUND:
+				playSound(pop());
+				break;
+			case INST_SET_PARTICLES:
+				spawnParticles(pop());
+				break;
+			case INST_LITERAL:
+			{
+				int value = bytecode[++i];
+				push(value);
+				break;
+			}
+			case INST_GET_HEALTH:
+			{
+				int wizard = pop();
+				push(getHealth(wizard));
+				break;
+			}
+			case INST_ADD:
+			{
+				int b = pop();
+				int a = pop();
+				push(a + b);
+				break;
+			}
+			}
+		}
 	}
-	render();
-}
+private:
+	static const int MAX_STACK = 128;
+	int stackSize_;
+	int stack_[MAX_STACK];
+
+private:
+	void push(int value)
+	{
+		assert(stackSize_ < MAX_STACK);
+		stack_[stackSize_++] = value;
+	}
+
+	int pop()
+	{
+		assert(stackSize_ > 0);
+		return stack_[--stackSize_];
+	}
+};
