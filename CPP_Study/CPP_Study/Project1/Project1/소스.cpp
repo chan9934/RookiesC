55e37161baca8c6ada21db8861c3c1e22bbbbca3
chan9934 <106580671+chan9934@users.noreply.github.com>
2023-09-12 07:57:06 +0900

    공부 타입 객체 패턴

--- CPP_Study/CPP_Study/Project1/Project1/소스.cpp
+++ CPP_Study/CPP_Study/Project1/Project1/소스.cpp
@@ -1,84 +1,12 @@
-
-
-enum Instruction
-{
-	INST_SET_HEALTH = 0x00,
-	INST_SET_WISDOM = 0x01,
-	INST_SET_AGILITY = 0x02,
-	INST_SET_SOUND = 0x03,
-	INST_SET_PARTICLES = 0x04,
-
-};
-
-
-class VM
+class Monster
 {
 public:
-	VM() : stackSize_(0) {}
-	void interpret(char bytecode[], int size)
-	{
-		for (int i = 0; i < size; ++i)
-		{
-			char instruction = bytecode[i];
+	virtual ~Monster() {}
+	virtual const char* getAttack() = 0;
 
-			switch (instruction)
-			{
-			case INST_SET_HEALTH:
-			{
-				int amount = pop();
-				int wizard = pop();
-				setHealth(wizard, amount);
-				break;
-			}
-			case INST_SET_WISDOM:
-				setWisdom(0, 100);
-				break;
-			case INST_SET_AGILITY:
-				setAgility(0, 100);
-				break;
-			case INST_SET_SOUND:
-				playSound(pop());
-				break;
-			case INST_SET_PARTICLES:
-				spawnParticles(pop());
-				break;
-			case INST_LITERAL:
-			{
-				int value = bytecode[++i];
-				push(value);
-				break;
-			}
-			case INST_GET_HEALTH:
-			{
-				int wizard = pop();
-				push(getHealth(wizard));
-				break;
-			}
-			case INST_ADD:
-			{
-				int b = pop();
-				int a = pop();
-				push(a + b);
-				break;
-			}
-			}
-		}
-	}
-private:
-	static const int MAX_STACK = 128;
-	int stackSize_;
-	int stack_[MAX_STACK];
+protected:
+	Monster(int startingHealth) : health_(startingHealth) {}
 
 private:
-	void push(int value)
-	{
-		assert(stackSize_ < MAX_STACK);
-		stack_[stackSize_++] = value;
-	}
-
-	int pop()
-	{a
-		assert(stackSize_ > 0);
-		return stack_[--stackSize_];
-	}
+	int health_;
 };
