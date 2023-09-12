3e4d0cee6e77aab7da1bd7a77bec630808c8ee1c
chan9934 <106580671+chan9934@users.noreply.github.com>
2023-09-13 08:38:14 +0900

    공부 컴포넌트 패턴

--- CPP_Study/CPP_Study/Project1/Project1/소스.cpp
+++ CPP_Study/CPP_Study/Project1/Project1/소스.cpp
@@ -1,12 +1,36 @@
-class Monster
+class Bjorn
 {
 public:
-	virtual ~Monster() {}
-	virtual const char* getAttack() = 0;
+	Bjorn() : velocity_(0), x_(0), y_(0) {}
+	void update(World& world, Graphics& graphics);
 
-protected:
-	Monster(int startingHealth) : health_(startingHealth) {}
 
 private:
-	int health_;
+	static const int WALK_ACCELERATION = 1;
+
+	int velocity_;
+	int x_, y_;
+
+	Volume volume_;
+
+	Sprite spriteStand_;
+	Sprite spriteWalkLeft_;
+	Sprite spriteWalkRight_;
 };
+class Volume
+{
+
+};
+class Sprite
+{
+
+};
+class World
+{
+
+};
+class Graphics
+{
+
+};
+
