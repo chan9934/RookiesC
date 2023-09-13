78d3d63c7231919f40178ba1eab6efa26d2489fb
chan9934 <106580671+chan9934@users.noreply.github.com>
2023-09-14 08:02:27 +0900

    공부 이벤트 큐

--- CPP_Study/CPP_Study/Project1/Project1/소스.cpp
+++ CPP_Study/CPP_Study/Project1/Project1/소스.cpp
@@ -1,36 +1,12 @@
-class Bjorn
+class Audio
 {
 public:
-	Bjorn() : velocity_(0), x_(0), y_(0) {}
-	void update(World& world, Graphics& graphics);
-
-
-private:
-	static const int WALK_ACCELERATION = 1;
-
-	int velocity_;
-	int x_, y_;
-
-	Volume volume_;
-
-	Sprite spriteStand_;
-	Sprite spriteWalkLeft_;
-	Sprite spriteWalkRight_;
+	static void playSound(SoundId id, int volume);
 };
-class Volume
+
+enum SoundId
 {
-
+	a,
+	b,
+	c
 };
-class Sprite
-{
-
-};
-class World
-{
-
-};
-class Graphics
-{
-
-};
-
