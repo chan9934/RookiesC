255fd080e6974165feffad484bdc2617e747048b
chan9934 <106580671+chan9934@users.noreply.github.com>
2023-09-20 08:12:01 +0900

    공부
    데이터 지역성

--- CPP_Study/CPP_Study/Project1/Project1/소스.cpp
+++ CPP_Study/CPP_Study/Project1/Project1/소스.cpp
@@ -6,3 +6,29 @@ public:
 	virtual void stopSound(int soundID) = 0;
 	virtual void stopAllSounds() = 0;
 };
+
+class ConsoleAudio : public Audio
+{
+	virtual void playSound(int soundID)
+	{
+
+	}
+	virtual void stopSound(int soundID)
+	{
+
+	}
+	virtual void stopAllSounds()
+	{
+
+	}
+};
+
+class Locator
+{
+public:
+	static Audio* getAudio() { return service_; }
+	static void provide(Audio* service) { service_ = service; }
+
+private:
+	static Audio* service_;
+};
