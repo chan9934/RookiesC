155a1611631c2e9f541d8714f0b9eea4006d85bb
chan9934 <106580671+chan9934@users.noreply.github.com>
2023-09-22 08:36:34 +0900

    공부 공간분할

--- CPP_Study/CPP_Study/Project1/Project1/소스.cpp
+++ CPP_Study/CPP_Study/Project1/Project1/소스.cpp
@@ -1,5 +1,36 @@
-class Particle
+
+class Unit
 {
+	friend class Grid;
+
 public:
-	Particle() : framesLeft
+	Unit(Grid* grid, double x, double y)
+		:grid_(grid), x_(x), y_(y)
+	{
+
+	}
+
+private:
+	double x_, y_;
+	Grid* grid_;
+};
+
+class Grid
+{
+
+public:
+	Grid()
+	{
+		for (int x = 0; x < NUM_CELLS; ++x)
+		{
+			for (int y = 0; y < NUM_CELLS; ++y)
+			{
+				cells_[x][y] = nullptr;
+			}
+		}
+	}
+	static const int NUM_CELLS = 10;
+	static const int CELL_SIZE = 20;
+private:
+	Unit* cells_[NUM_CELLS][NUM_CELLS];
 };
